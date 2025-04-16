c40eda1e1caae966071bc19a3b729c622dc35e51
Nakib00 <2022142@iub.edu.bd>
2025-04-16 14:07:20 +0600

    update

--- config/cors.php
+++ config/cors.php
@@ -1,18 +1,15 @@
 <?php
 
 return [
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'https://hub.desklago.com',
         'http://localhost:3000',
         'http://localhost:5173',
-        'https://hub.desklago.com'
     ],
-
-    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
@@ -21,5 +18,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => true,
-
 ];
