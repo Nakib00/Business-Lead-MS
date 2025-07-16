31faa0685698a8f55f276e7df0b28aa2056a146f
Nakib00 <2022142@iub.edu.bd>
2025-07-16 22:24:37 +0600

    up

--- database/migrations/2025_07_11_181739_create_form_submissions_table.php
+++ database/migrations/2025_07_11_181739_create_form_submissions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('form_id')->constrained()->onDelete('cascade');
             $table->unsignedBigInteger('submitted_by')->nullable();
+            $table->unsignedBigInteger('admin_id')->nullable();
             $table->timestamps();
         });
     }
