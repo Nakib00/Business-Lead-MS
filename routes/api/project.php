cb60f14e12d137a10844803ab692ee2f62669d66
Nakib00 <2022142@iub.edu.bd>
2025-10-20 22:39:58 +0600

    fdsafds

--- routes/api/project.php
+++ routes/api/project.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Project\ProjectController;
 
-// Route::middleware('auth:api')->group(function () {
+Route::middleware('auth:api')->group(function () {
     Route::prefix('projects')->group(function () {
         Route::post('/', [ProjectController::class, 'store']);
     });
-// });
+});
