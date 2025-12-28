62e89a4899cde27ac67f7967fb96da396dbb6a7d
Nakib00 <2022142@iub.edu.bd>
2025-12-28 22:21:01 +0600

    feat: add TrustProxies middleware and configure CORS settings.

--- config/cors.php
+++ config/cors.php
@@ -11,11 +11,13 @@ return [
         'http://localhost:5173'
     ],
 
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['Authorization'],
 
-    'max_age' => 0,
+    'max_age' => 86400, // Cache preflight for 24 hours
 
     'supports_credentials' => true,
 ];
