931e9511d0ef75c08fbcd094010c7c1022456b3d
Nakib00 <2022142@iub.edu.bd>
2026-01-14 23:56:13 +0600

    ds

--- config/cors.php
+++ config/cors.php
@@ -5,13 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://hub.desklago.com',
-        'https://hub.desklago.com/',
-        'http://localhost:3000',
-        'http://localhost:5173',
-        'http://localhost:5174'
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
