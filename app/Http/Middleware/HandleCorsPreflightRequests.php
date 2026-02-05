89f16972f60759d5d6e22a7cafa71fc18fa5a2df
Nakib00 <2022142@iub.edu.bd>
2026-02-05 21:36:17 +0600

    feat: Add middleware to handle CORS preflight requests and set appropriate headers.

--- app/Http/Middleware/HandleCorsPreflightRequests.php
+++ app/Http/Middleware/HandleCorsPreflightRequests.php
@@ -20,7 +20,7 @@ class HandleCorsPreflightRequests
         if ($request->isMethod('OPTIONS')) {
             return response('', 200)
                 ->header('Access-Control-Allow-Origin', $this->getAllowedOrigin($request))
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With')
                 ->header('Access-Control-Allow-Credentials', 'true')
                 ->header('Access-Control-Max-Age', '86400');
